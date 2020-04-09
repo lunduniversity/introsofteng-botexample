a8ce187840203be8e1a7a5906c69f770ce9f53c1
Markus Borg <markus.borg@gmail.com>
2020-04-09 15:25:53 +0200

    Reorganize into BasicBots

---
+++ BasicBots/src/main/java/basicdroid/BasicDroid.java
@@ -0,0 +1,148 @@
+/**	
+Copyright (c) 2017 Markus Borg
+Building on work by Mathew A. Nelson and Robocode contributors.
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+package basicdroid;
+
+import etsa03.*;
+
+
+import robocode.Droid;
+import robocode.MessageEvent;
+import robocode.TeamRobot;
+import robocode.util.Utils;
+import static robocode.util.Utils.normalRelativeAngleDegrees;
+import java.io.IOException;
+
+import robocode.BulletHitEvent;
+
+
+/**
+ *  BasicDroid - a simple droid that fires based on leader bot's orders
+ *
+ * @author Markus Borg
+ * @author Teodor Ahlinder, improvements for LU Rumble (2020)
+ */
+public class BasicDroid extends TeamRobot implements Droid {
+
+	/**
+	 * run:  Droid's default behavior
+	 */
+	public void run() {
+		out.println("MyFirstDroid ready.");
+	}
+
+	/**
+	 * onMessageReceived:  What to do when our leader sends a message
+	 */
+	public void onMessageReceived(MessageEvent e) {
+		// Set our colors
+		if (e.getMessage() instanceof RobotColors) {
+			RobotColors c = (RobotColors) e.getMessage();
+			setBodyColor(c.bodyColor);
+			setGunColor(c.gunColor);
+			setRadarColor(c.radarColor);
+			setScanColor(c.scanColor);
+			setBulletColor(c.bulletColor);
+		} else {
+			MessageReader reader = new MessageReader((String)e.getMessage());
+			if (reader.getMoveTo() != null) goTo(reader.getMoveTo());
+			
+			// If enemy position, fire!
+			String[] values = reader.getEnemyDetails();
+			if (values.length > 0) {
+				String[] ss = values[0].split(";");
+				Point p = null;
+				try {
+					double energy = Double.parseDouble(ss[4]);
+					if (energy <= 0) {
+						return;
+					}
+					int x = Integer.parseInt(ss[1]);
+					int y = Integer.parseInt(ss[2]);
+					p = new Point(x,y);
+				} catch (RuntimeException err) {}
+				if (p != null) fireAtPoint(p);
+			}
+		}
+	}
+	
+	/**
+	 * onHitBullet:  What to do when out bullet hits a robot
+	 */
+	@SuppressWarnings("unused")
+	private void onHitBullet(BulletHitEvent e) {
+		if (e.getEnergy() <= 0) {
+			try {
+				// Message allies that enemy bot is dead
+				MessageWriter writer = new MessageWriter();
+				writer.addEnemyDetails(e.getName(), 0, 0, 0, e.getEnergy(), 0, 0);
+				broadcastMessage(writer.composeMessage());
+			} catch (IOException ignored) {}
+		}
+	}
+	
+	private void fireAtPoint(Point p) {
+		// Calculate x and y to target
+		double dx = p.getX() - this.getX();
+		double dy = p.getY() - this.getY();
+		// Calculate angle to target
+		double theta = Math.toDegrees(Math.atan2(dx, dy));
+
+		// Turn gun to target
+		turnGunRight(normalRelativeAngleDegrees(theta - getGunHeading()));
+		// Fire hard!
+		fire(3);
+	}
+	
+	/**
+	 * Set the robot to go to a certain point. 
+	 * The code comes from <a href="http://robowiki.net/wiki/GoTo"> http://robowiki.net/wiki/GoTo </a>
+	 * @param destination coordinates of the destination
+	 */
+	private void goTo(Point destination) {
+		/* Transform our coordinates into a vector */
+		double x = destination.getX();
+		double y = destination.getY();
+		x -= getX();
+		y -= getY();
+	 
+		/* Calculate the angle to the target position */
+		double angleToTarget = Math.atan2(x, y);
+	 
+		/* Calculate the turn required get there */
+		double targetAngle = Utils.normalRelativeAngle(angleToTarget - getHeadingRadians());
+	 
+		/* 
+		 * The Java Hypot method is a quick way of getting the length
+		 * of a vector. Which in this case is also the distance between
+		 * our robot and the target location.
+		 */
+		double distance = Math.hypot(x, y);
+	 
+		/* This is a simple method of performing set front as back */
+		double turnAngle = Math.atan(Math.tan(targetAngle));
+		setTurnRightRadians(turnAngle);
+		if(targetAngle == turnAngle) {
+			setAhead(distance);
+		} else {
+			setBack(distance);
+		}
+	}
+}
---
+++ BasicBots/src/main/java/basicleaderbot/BasicLeaderBot.java
@@ -0,0 +1,215 @@
+/**	
+Copyright (c) 2020 Teodor Ahlinder
+
+Building on work by Mathew A. Nelson and Robocode contributors.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+package basicleaderbot;
+
+import robocode.HitByBulletEvent;
+import robocode.MessageEvent;
+import robocode.ScannedRobotEvent;
+import robocode.TeamRobot;
+import etsa03.*;
+import java.awt.*;
+import java.io.IOException;
+import java.util.ArrayList;
+
+/**
+ * BasicLeaderBot (BLB) - a sample team robot for ETSA03.
+ * 
+ * Looks around for enemies, and orders team mates to fire.
+ * If hit by a bullet, it sometimes changes its movement.
+ *
+ * @author Markus Borg
+ * @author Teodor Ahlinder, improvements for LU Rumble (2020)
+ */
+public class BasicLeaderBot extends TeamRobot {
+	private static final Color BODYCOLOR = Color.white;
+	private static final Color GUNCOLOR = Color.white;
+	private static final Color RADARCOLOR = Color.white;
+	private static final Color SCANCOLOR = Color.white;
+	private static final Color BULLETCOLOR = Color.white;
+	
+	private ArrayList<DummyRobot> knownEnemyRobots;
+	private ArrayList<DummyRobot> knownAlliedRobots;
+	
+	public void run() {
+		// ----------------------------------------------
+		// ------------- Starting behavior --------------
+		// ----------------------------------------------
+		
+		// -------- Configuring list of enemies ---------
+		knownEnemyRobots = new ArrayList<DummyRobot>();
+		
+		// ---------- Asserting leader control ----------
+		try {
+			// Broadcast to team that this bot is the leader
+			MessageWriter writer = new MessageWriter();
+			writer.addLeadership("followMe");
+			broadcastMessage(writer.composeMessage());
+		} catch (IOException ignored) {}
+		
+		// ----------- Setting team colors --------------
+		RobotColors c = new RobotColors();
+		c.bodyColor = BODYCOLOR;
+		c.gunColor = GUNCOLOR;
+		c.radarColor = RADARCOLOR;
+		c.scanColor = SCANCOLOR;
+		c.bulletColor = BULLETCOLOR;
+
+		// Set the color of BLB
+		setBodyColor(c.bodyColor);
+		setGunColor(c.gunColor);
+		setRadarColor(c.radarColor);
+		setScanColor(c.scanColor);
+		setBulletColor(c.bulletColor);
+		try {
+			// Send RobotColors object to the entire team
+			broadcastMessage(c);
+		} catch (IOException ignored) {
+			System.out.println("Could not broadcast team colors.");
+		}
+				
+		// ----------------------------------------------
+		// ------------- Running Behavior ---------------
+		// ----------------------------------------------
+		
+		// Default behavior - BLB's standard sequence
+		while (true) {
+			setTurnRadarRight(10000);
+			ahead(100);
+			back(100);
+		}
+	}
+
+	/**
+	 * onScannedRobot: BLB has detected another robot. Update internal lists and transmit them to team.
+	 */
+	public void onScannedRobot(ScannedRobotEvent e) {
+		// Calculate robot bearing
+		double robotBearing = this.getHeading() + e.getBearing();
+		
+		// Calculate robot's position
+		double robotX = getX() + e.getDistance() * Math.sin(Math.toRadians(robotBearing));
+		double robotY = getY() + e.getDistance() * Math.cos(Math.toRadians(robotBearing));
+		
+		// Store robot in appropriate list
+		String scannedName = e.getName();
+		DummyRobot r = new DummyRobot(scannedName, robotX, robotY, e.getVelocity(), e.getEnergy(), e.getHeading(), 0);
+		if (isTeammate(scannedName)) {
+			updateList(r, knownAlliedRobots);
+		} else {
+			updateList(r, knownEnemyRobots);
+		}
+		
+		try {
+			// Send positions of known allies to team
+			MessageWriter writer = new MessageWriter();
+			writer.addMyPos(this.getX(), this.getY());
+			for (DummyRobot allies : knownAlliedRobots) {
+				writer.addFriendPos(allies.getX(), allies.getY());
+			}
+			// Send positions of known enemies to team
+			for (DummyRobot enemies : knownEnemyRobots) {
+				writer.addEnemyDetails(enemies.getName(), enemies.getX(), enemies.getY(), enemies.getVelocity(), enemies.getEnergy(), enemies.getHeading(), enemies.getGunHeading());
+			}
+			broadcastMessage(writer.composeMessage());
+		} catch (IOException ex) {
+			out.println("Unable to send order: ");
+			ex.printStackTrace(out);
+		}
+	}
+
+	/**
+	 * Adds a new robot to the list, or updates the robots values if it is already in the list
+	 * @param r robot to add or update
+	 * @return 0 if robot was already in list, 1 if it was added as a new robot, -1 if robot energy was <=0
+	 */
+	private int updateList(DummyRobot r, ArrayList<DummyRobot> list) {
+		for (int i = 0; i < list.size(); i++) {
+			DummyRobot robot = list.get(i);
+			if (r.getName() == robot.getName()) {
+				if (r.getEnergy() <= 0) {
+					list.remove(i);
+					return -1;
+				}
+				list.get(i).update(r.getX(), r.getY(), r.getVelocity(), r.getEnergy(), r.getHeading(), r.getGunHeading());
+				return 0;
+			}
+		}
+		for (DummyRobot robot : list) {
+			if (r.getName() == robot.getName()) {
+				robot = r;
+				return 0;
+			}
+		}
+		list.add(r);
+		return 1;
+	}
+
+	/**
+	 * onHitByBullet: BLB has been hit by a bullet. Turn perpendicular to path of the bullet.
+	 */
+	public void onHitByBullet(HitByBulletEvent e) {
+		turnLeft(90 - e.getBearing());
+	}
+	
+	/**
+	 * onDeath: Code executed when this robot dies.
+	 */
+	public void onDeath() {
+		try {
+			// Declare to team that this bot is dead and should not be followed anymore
+			MessageWriter writer = new MessageWriter();
+			writer.addLeadership("leadMe");
+			broadcastMessage(writer.composeMessage());
+		} catch (IOException ignored) {}
+	}
+	
+	public void onMessageReceived(MessageEvent e) {
+		MessageReader reader = new MessageReader((String)e.getMessage());
+		String[] enemies = reader.getEnemyDetails();
+		for (String values : enemies) {
+			String[] v = values.split(";");
+			if (v.length >= 7) {
+				try {
+					double x = Double.parseDouble(v[1]);
+					double y = Double.parseDouble(v[2]);
+					double velocity = Double.parseDouble(v[3]);
+					double energy = Double.parseDouble(v[4]);
+					double heading = Double.parseDouble(v[5]);
+					double gunHeading = Double.parseDouble(v[6]);
+					DummyRobot r = new DummyRobot(v[0], x, y, velocity, energy, heading, gunHeading);
+					updateList(r, knownEnemyRobots);
+				} catch (RuntimeException err) {}
+			}
+		}
+	}
+
+	
+	public ArrayList<DummyRobot> getKnownEnemyRobots() {
+		return knownEnemyRobots;
+	}
+
+	public ArrayList<DummyRobot> getKnownAlliedRobots() {
+		return knownAlliedRobots;
+	}
+}
